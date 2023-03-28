7125207e8aa601dccd5795d4453cb7f26bef1cf1
nameiswaiyinsin <waiyinsin@gmail.com>
2023-03-28 22:22:48 +0000

    Add a destructuring and reduce() method challenge

--- reduce.js
+++ reduce.js
@@ -68,3 +68,43 @@ let experienceByProfession = teamMembers.reduce((acc, curr) => {
 }, {});
 
 console.log(experienceByProfession);
+
+// Challenge 1: Find the student with the highest english score and present as '{ name: 'Adam', max: 88 }'
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 73, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+//write code here:
+// Using destructuring 
+const biggest = students.reduce(({max, name}, {name:n, results:{english}}) => {
+  if(max < english) {
+      acc = {name:n, max: english};
+  }
+  return acc;
+  }, {name: '', max: 0});
+  
+console.log(biggest);
